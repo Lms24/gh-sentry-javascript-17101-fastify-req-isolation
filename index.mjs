30ca5b50f6e79466735438f63c6d63943e61c674
Lukas Stracke <lukas.stracke@sentry.io>
2025-07-29 13:29:46 +0200

    test shouldHandleError

--- index.mjs
+++ index.mjs
@@ -18,7 +18,26 @@ app.get("/test-error", (request, reply) => {
   throw new Error("test error");
 });
 
+app.get("/test-ignore-error", (request, reply) => {
+  throw Response.json(
+    {
+      error: {
+        code: 400,
+        details: "test ignore error",
+        id: "some id",
+        message: "some message",
+      },
+    },
+    {
+      headers: {},
+      status: 400,
+    }
+  );
+});
+
 const address = await app.listen({
   host: "::",
   port: 3000,
 });
+
+console.log(`Server is running on ${address}`);
--- instrument.mjs
+++ instrument.mjs
@@ -4,12 +4,20 @@ Sentry.init({
   dsn: "https://b5f29bfe807c4898b0f4a155c797c936@o447951.ingest.us.sentry.io/4505583202074624",
   tracesSampleRate: 1.0,
   debug: true,
+  integrations: [
+    Sentry.fastifyIntegration({
+      shouldHandleError: (e) => {
+        console.log("fastify shouldHandleError", e);
+        return false;
+      },
+    }),
+  ],
   beforeSendTransaction: (txn) => {
     console.log("beforeSendTransaction", txn.contexts.trace);
     return txn;
   },
   beforeSend: (event) => {
-    console.log("beforeSend", event.contexts.trace);
+    console.log("beforeSend", event.exception?.values[0]);
     return event;
   },
 });
